8d5267464eda840a8bbcd85a8d8915313bbdb456
YKGio <aresr417@gmail.com>
2023-05-18 14:41:14 +0800

    model update

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,7 +9,7 @@ module StringofFate
       @id = link_info['attributes']['id']
       @name = link_info['attributes']['nickname']
       @repo_url = link_info['attributes']['url']
-      @platform_id = link_info['platform']['id']
+      @platform_id = link_info['platform']['attributes']['id']
     end
   end
 end
