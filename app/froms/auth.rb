77f257c05831f59a6f67c0559ce5a3823ae05879
YKGio <aresr417@gmail.com>
2023-05-29 18:29:08 +0800

    create forms

---
+++ app/froms/auth.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require_relative 'form_base'
+
+module StringofFate
+  module Form
+    class LoginCredentials < Dry::Validation::Contract
+      params do
+        required(:username).filled
+        required(:password).filled
+      end
+    end
+
+    class Registration < Dry::Validation::Contract
+      config.messages.load_paths << File.join(__dir__, 'errors/account_details.yml')
+
+      params do
+        required(:username).filled(format?: USERNAME_REGEX, min_size?: 4)
+        required(:email).filled(format?: EMAIL_REGEX)
+      end
+    end
+
+    class Passwords < Dry::Validation::Contract
+      config.messages.load_paths << File.join(__dir__, 'errors/password.yml')
+
+      params do
+        required(:password).filled
+        required(:password_confirm).filled
+      end
+
+      def enough_entropy?(string)
+        StringSecurity.entropy(string) >= 3.0
+      end
+
+      rule(:password) do
+        key.failure('Password must be more complex') unless enough_entropy?(value)
+      end
+
+      rule(:password, :password_confirm) do
+        key.failure('Passwords do not match') unless values[:password].eql?(values[:password_confirm])
+      end
+    end
+  end
+end
