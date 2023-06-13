add75e41ac394d9d0f2ec3d17d0b2306055d7ad7
YKGio <aresr417@gmail.com>
2023-06-13 17:20:04 +0800

    sign protec create

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'roda'
+require_relative './app'
 
 module StringofFate
   # Web controller for String of Fate API
@@ -11,11 +12,11 @@ module StringofFate
       # GET /links/[link_id]
       routing.get(String) do |link_id|
         link_info = GetLink.new(App.config)
-                              .call(@current_account, link_id)
+                           .call(@current_account, link_id)
         link = Link.new(link_info)
 
         view :link, locals: {
-          current_account: @current_account, link: link
+          current_account: @current_account, link:
         }
       end
     end
