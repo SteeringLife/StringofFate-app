69843a27a3bc8974cdd672ff07e02dabe86de115
YKGio <aresr417@gmail.com>
2023-06-15 01:17:28 +0800

    bug fix

--- app/services/give_card.rb
+++ app/services/give_card.rb
@@ -14,11 +14,10 @@ module StringofFate
     end
 
     def call(current_account:, receiver:, card_id:)
-      puts "GiveCard: #{current_account.inspect} #{receiver.inspect} #{card_id}"
       response = HTTP.auth("Bearer #{current_account.auth_token}")
                      .put("#{api_url}/cards/#{card_id}/receivers",
                           json: { email: receiver[:email] })
-      puts "GiveCard: #{response.inspect}"
+
       raise CardNotGave unless response.code == 200
     end
   end
