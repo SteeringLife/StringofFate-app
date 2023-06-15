2a7c651a4c6c41c35a8e5394e2ef55c6a75ed5fa
YKGio <aresr417@gmail.com>
2023-06-15 14:46:43 +0800

    public hashtag update

--- app/services/create_new_public_hashtag.rb
+++ app/services/create_new_public_hashtag.rb
@@ -5,6 +5,8 @@ require 'http'
 module StringofFate
   # Create a new configuration file for a card
   class CreateNewPublicHashtag
+    class AlreadyExistsError < StandardError; end
+
     def initialize(config)
       @config = config
     end
@@ -13,10 +15,11 @@ module StringofFate
       @config.API_URL
     end
 
-    def call(current_account:, card_id:, public_hashtag_data:)
-      config_url = "#{api_url}/cards/#{card_id}/public_hashtags"
+    def call(current_account:, public_hashtag_data:)
+      config_url = "#{api_url}/public_hashtags"
       response = HTTP.auth("Bearer #{current_account.auth_token}")
                      .post(config_url, json: public_hashtag_data)
+      raise AlreadyExistsError if response.code == 409
 
       response.code == 201 ? JSON.parse(response.body.to_s) : raise
     end
