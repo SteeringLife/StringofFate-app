89233fe6a47afd443dc4f242a78beaef61dbebdf
YKGio <aresr417@gmail.com>
2023-06-15 16:47:15 +0800

    tag search update

---
+++ app/services/get_cards_by_tag.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'http'
+
+module StringofFate
+  # Returns all cards belonging to an account
+  class GetCardsByTag
+    def initialize(config)
+      @config = config
+    end
+
+    def call(current_account, tag_content)
+      response = HTTP.auth("Bearer #{current_account.auth_token}")
+                     .get("#{@config.API_URL}/cards/tags/#{tag_content}}")
+
+      response.code == 200 ? JSON.parse(response.to_s)['data'] : nil
+    end
+  end
+end
